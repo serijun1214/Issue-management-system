992839ad3cf8ad9b58405179dc38b38cbc148cb4
Junki Seri <junki.seri@rakus-partners.co.jp>
2023-10-26 23:50:04 +0900

    課題詳細画面の表示の実装が完了

--- src/main/java/com/example/issuemanagementsystem/web/issue/IssueController.java
+++ src/main/java/com/example/issuemanagementsystem/web/issue/IssueController.java
@@ -8,6 +8,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -37,6 +38,13 @@ public class IssueController {
 
         issueService.create(form.getSummary(), form.getDescription());
         return "redirect:/issues";
+    }
+
+    @GetMapping("/{issueId}")
+    public String showDetail(@PathVariable("issueId") long issueId, Model model) {
+
+        model.addAttribute("issue", issueService.findById(issueId));
+        return "issues/detail";
     }
 
 }
