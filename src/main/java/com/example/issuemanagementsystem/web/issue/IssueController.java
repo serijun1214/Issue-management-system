8522f836d6cceae5f7f4c3a1f1b664733d87815c
Junki Seri <junki.seri@rakus-partners.co.jp>
2023-10-26 22:34:00 +0900

    フォームクラスの追加

--- src/main/java/com/example/issuemanagementsystem/web/issue/IssueController.java
+++ src/main/java/com/example/issuemanagementsystem/web/issue/IssueController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,6 +24,11 @@ public class IssueController {
     @GetMapping("/creationForm")
     public String showCreationForm() {
         return "issues/creationForm";
+    }
+
+    @PostMapping("")
+    public String create(IssueForm form, Model model) {
+        return showList(model);
     }
 
 }
---
+++ src/main/java/com/example/issuemanagementsystem/web/issue/IssueForm.java
@@ -0,0 +1,12 @@
+package com.example.issuemanagementsystem.web.issue;
+
+import lombok.Data;
+
+@Data
+public class IssueForm {
+
+    //概要
+    private String summary;
+    //説明
+    private String description;
+}
