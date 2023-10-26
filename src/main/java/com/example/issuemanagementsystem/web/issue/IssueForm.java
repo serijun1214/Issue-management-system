7895f61d354d3dc172ecc1041b8c2b9c1931875d
Junki Seri <junki.seri@rakus-partners.co.jp>
2023-10-26 23:18:49 +0900

    バリデーションを実装

--- src/main/java/com/example/issuemanagementsystem/web/issue/IssueForm.java
+++ src/main/java/com/example/issuemanagementsystem/web/issue/IssueForm.java
@@ -1,12 +1,18 @@
 package com.example.issuemanagementsystem.web.issue;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class IssueForm {
 
     //概要
+    @NotBlank
+    @Size(max = 256)
     private String summary;
     //説明
+    @NotBlank
+    @Size(max = 256)
     private String description;
 }
