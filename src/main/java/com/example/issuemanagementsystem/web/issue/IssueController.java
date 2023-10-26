4cdacac6a1931073950eb5db6044abb7f877195c
Junki Seri <junki.seri@rakus-partners.co.jp>
2023-10-26 22:25:58 +0900

    creationForm.htmlを作成しフォーム画面を作成

--- src/main/java/com/example/issuemanagementsystem/web/issue/IssueController.java
+++ src/main/java/com/example/issuemanagementsystem/web/issue/IssueController.java
@@ -5,18 +5,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/issues")
 @RequiredArgsConstructor
 public class IssueController {
 
     private final IssueService issueService;
 
-    // GET /issue
-    @GetMapping("/issues")
+    @GetMapping("")
     public String showList(Model model) {
-
         model.addAttribute("issueList", issueService.findAll());
         return "issues/list";
     }
+
+    @GetMapping("/creationForm")
+    public String showCreationForm() {
+        return "issues/creationForm";
+    }
+
 }
