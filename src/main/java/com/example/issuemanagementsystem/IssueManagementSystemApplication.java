4823e87d4603b7bc7d3960d1c949c4756b6d16b5
Junki Seri <junki.seri@rakus-partners.co.jp>
2023-10-26 10:34:02 +0900

    first commit

---
+++ src/main/java/com/example/issuemanagementsystem/IssueManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.issuemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/issuemanagementsystem/IssueManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.issuemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
